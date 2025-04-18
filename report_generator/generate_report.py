f94316ce53a89fb66b01133b55ad27952271916d
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2025-04-18 21:03:39 +0530

    updates to output path in generate_report.py

--- report_generator/generate_report.py
+++ report_generator/generate_report.py
@@ -5,10 +5,14 @@ from jinja2 import Template
 def generate_pdf(data, output='report.pdf'):
     with open('report_generator/report_template.html', 'r', encoding='utf-8') as f:
         template = Template(f.read())
-
+    
+    output_dir = 'scan_reports'
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, output)
+    
     rendered = template.render(results=data)
 
     config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
 
-    pdfkit.from_string(rendered, os.path.join('scan_reports', output), configuration=config)
+    pdfkit.from_string(rendered, output_path, configuration=config)
     print(f"[+] Styled PDF report saved as {output}")
